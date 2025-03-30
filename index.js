c0ef10bc53c8adf75fd12229c848381854558be5
Cephas Mwai <cephasmwai561@gmail.com>
2025-03-30 11:48:12 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here!
+
+const main = document.querySelector('main#main');
+if (main) {
+  main.remove();
+}
+
+
+const newHeader = document.createElement('h1');
+
+
+newHeader.id = 'victory';
+
+
+newHeader.textContent = 'YOUR-NAME is the champion'; // Replace YOUR-NAME with your name or desired text
+
+
+document.body.appendChild(newHeader);
